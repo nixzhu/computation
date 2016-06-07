06349c21728904c135484d1f9def2eb87996d7fd
nixzhu <zhuhongxu@gmail.com>
2016-06-07 20:12:40 +0800

    Tape

---
+++ TuringMachine/Tape.swift
@@ -0,0 +1,10 @@
+
+// @nixzhu (zhuhongxu@gmail.com)
+
+struct Tape {
+
+    var leftCharacters: [Character]
+    let middleCharacter: Character
+    var rightCharacters: [Character]
+    let blankCharacter: Character
+}
---
+++ TuringMachine/main.swift
@@ -0,0 +1,5 @@
+
+// @nixzhu (zhuhongxu@gmail.com)
+
+let tape = Tape(leftCharacters: ["1", "0", "1"], middleCharacter: "1", rightCharacters: [], blankCharacter: "_")
+print(tape.middleCharacter)
