9bc5c3d2c17371e65cbda6041d085d136c3371f0
nixzhu <zhuhongxu@gmail.com>
2016-06-07 20:30:38 +0800

    Tape write & move

--- TuringMachine/Tape.swift
+++ TuringMachine/Tape.swift
@@ -3,8 +3,31 @@
 
 struct Tape {
 
-    var leftCharacters: [Character]
+    let leftCharacters: [Character]
     let middleCharacter: Character
-    var rightCharacters: [Character]
+    let rightCharacters: [Character]
     let blankCharacter: Character
+
+    func writeCharacter(character: Character) -> Tape {
+        return Tape(leftCharacters: leftCharacters, middleCharacter: character, rightCharacters: rightCharacters, blankCharacter: blankCharacter)
+    }
+
+    func moveHeadLeft() -> Tape {
+        var _leftCharacters = leftCharacters
+        let leftLast: Character? = _leftCharacters.isEmpty ? nil : _leftCharacters.removeLast()
+        return Tape(leftCharacters: _leftCharacters, middleCharacter: leftLast ?? blankCharacter, rightCharacters: [middleCharacter] + rightCharacters, blankCharacter: blankCharacter)
+    }
+
+    func moveHeadRight() -> Tape {
+        var _rightCharacters = rightCharacters
+        let rightFirst: Character? = _rightCharacters.isEmpty ? nil : _rightCharacters.removeFirst()
+        return Tape(leftCharacters: leftCharacters + [middleCharacter], middleCharacter: rightFirst ?? blankCharacter, rightCharacters: _rightCharacters, blankCharacter: blankCharacter)
+    }
+}
+
+extension Tape: CustomStringConvertible {
+
+    var description: String {
+        return "Tape: " + leftCharacters.map({ "\($0)" }).joinWithSeparator("") + "(\(middleCharacter))" + rightCharacters.map({ "\($0)" }).joinWithSeparator("")
+    }
 }
--- TuringMachine/main.swift
+++ TuringMachine/main.swift
@@ -2,4 +2,8 @@
 // @nixzhu (zhuhongxu@gmail.com)
 
 let tape = Tape(leftCharacters: ["1", "0", "1"], middleCharacter: "1", rightCharacters: [], blankCharacter: "_")
-print(tape.middleCharacter)
+print(tape)
+print(tape.moveHeadLeft())
+print(tape.writeCharacter("0"))
+print(tape.moveHeadRight())
+print(tape.moveHeadRight().writeCharacter("0"))
