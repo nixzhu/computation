0c6bfafb17408cbadd4a4c8c51ca5d01b6f0afab
nixzhu <zhuhongxu@gmail.com>
2016-06-11 15:34:07 +0800

    toInteger

--- UntypedLambdaCalculus/main.swift
+++ UntypedLambdaCalculus/main.swift
@@ -1,28 +1,37 @@
 
 // @nixzhu (zhuhongxu@gmail.com)
 
-typealias P = () -> Void
+typealias P = Int -> Int
 
 let zero = { (p: P) in
-    return { (x: Void) in
+    return { (x: Int) in
         return x
     }
 }
 
-print(zero)
-
 let one = { (p: P) in
-    return { (x: Void) in
+    return { (x: Int) in
         return p(x)
     }
 }
 
-print(one)
-
 let two = { (p: P) in
-    return { (x: Void) in
+    return { (x: Int) in
         return p(p(x))
     }
 }
 
-print(two)
+let three = { (p: P) in
+    return { (x: Int) in
+        return p(p(p(x)))
+    }
+}
+
+func toInteger(proc: P -> (Int -> Int)) -> Int {
+    return proc({ $0 + 1 })(0)
+}
+
+print(toInteger(zero))
+print(toInteger(one))
+print(toInteger(two))
+print(toInteger(three))
